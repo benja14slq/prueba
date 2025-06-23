84f7fa3e6b6fac55436941fdbbfdba60ad149579
benja14slq <benjamin.betty@sansano.usm.cl>
2025-06-22 21:51:53 -0400

    .

--- config/settings.py
+++ config/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool) #True 
 #config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') #['*']
 #os.getenv('ALLOWED_HOSTS', '').split(',')
 
 
